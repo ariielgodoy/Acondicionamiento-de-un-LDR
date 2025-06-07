c80a23b618e69fb14ed80b9e3610faf4a3758bd1
ariielgodoy <165669544+ariielgodoy@users.noreply.github.com>
2025-06-07 23:34:27 +0200

    .VI y código en arduino
    Sirve para la comunicación entre el arduino y el software labview por el puerto serie

---
+++ ldr_arduino.ino
@@ -0,0 +1,42 @@
+ char c = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(4, INPUT);
+  analogReadResolution(12);         // Resolución de 12 bits (0-4095)
+  analogSetAttenuation(ADC_11db);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int lectura = analogRead(4);
+  //float voltaje = (lectura / 4095.0)*3;
+
+  //Serial.print("ADC: ");
+  //Serial.println(lectura);
+  //Serial.print(" | Voltaje: ");
+  //Serial.print(voltaje, 3);
+  //Serial.println(" V");
+  //double lux = 8842.7*voltaje*voltaje-51148*voltaje+74508;
+  //Serial.print(lux);
+  //Serial.println(" lux");
+  if (Serial.available()>0)
+  {
+    c = Serial.read();
+    
+    if(c == 'D'){
+      int lectura = analogRead(4);
+      Serial.print(lectura);
+      delay(50);
+    }
+    else
+    {
+      Serial.print("X");
+      delay(1000);
+    }
+  }
+
+  
+
+}
